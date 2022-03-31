1b37b22b2859e0125536eaf5766cd0bdfdf20a88
user1 <akwaseem24@gmail.com>
2022-03-31 17:45:57 +0530

    intial commit

--- index.js
+++ index.js
@@ -17,8 +17,38 @@ function changecontent() {
 function changecontent() {
     document.getElementById("demo1").innerHTML = "Hello!! A msg by EPIT";
 }
-let value = { x: "5", y: "2", a: "EP", b: "IT" };
-document.getElementById('plus ').innerHTML = value.x + y;
-document.getElementById('multi ').innerHTML = x * y;
-document.getElementById('assign ').innerHTML = x += 4;
-document.getElementById('prod ').innerHTML = x / y;
+const value = {
+    x: 3,
+    y: 6,
+    a: "EP",
+    b: "IT",
+    add: function() {
+        return this.x + this.y;
+    },
+    mult: function() {
+        return this.x * this.y;
+    },
+    assig: function() {
+        return this.x += 10;
+    },
+    prod: function() {
+        return this.x / this.y;
+    },
+    con: function() {
+        return this.a + " " + this.b;
+    },
+    per: function() {
+        return this.x % this.y;
+    },
+    sub: function() {
+        return this.x - this.y;
+    }
+
+};
+document.getElementById('plus ').innerHTML = value.add();
+document.getElementById('multi ').innerHTML = value.mult();
+document.getElementById('assign ').innerHTML = value.assig();
+document.getElementById('prod ').innerHTML = value.prod();
+document.getElementById('con ').innerHTML = value.con();
+document.getElementById('per ').innerHTML = value.per();
+document.getElementById('sub ').innerHTML = value.sub();
