e0fbfab95ebac920d0097a87811cabee510c510f
user1 <akwaseem24@gmail.com>
2022-03-31 16:29:37 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+function fontsize() {
+    document.getElementById("demo1").style.fontSize = "35px";
+}
+
+function hidecontent() {
+    document.getElementById("demo1").style.display = "none";
+}
+
+function showcontent() {
+    document.getElementById("demo1").style.display = "block";
+}
+
+function changecontent() {
+    document.getElementById("demo1").innerHTML = "Hello!! A msg by EPIT";
+}
+
+function changecontent() {
+    document.getElementById("demo1").innerHTML = "Hello!! A msg by EPIT";
+}
+let value = { x: "5", y: "2", a: "EP", b: "IT" };
+document.getElementById('plus ').innerHTML = value.x + y;
+document.getElementById('multi ').innerHTML = x * y;
+document.getElementById('assign ').innerHTML = x += 4;
+document.getElementById('prod ').innerHTML = x / y;
